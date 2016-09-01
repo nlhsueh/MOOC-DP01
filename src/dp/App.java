bcc26833ce3fabc69dd8ceee026ea57e4cfebc77
nlhsueh <nlhsueh@fcu.edu.tw>
2016-09-01 14:05:16 +0800

    no message

---
+++ src/dp/App.java
@@ -0,0 +1,17 @@
+package dp;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Employee {
+}
+
+class Engineer extends Employee {
+	
+}
---
+++ src/factory_method/App.java
@@ -0,0 +1,10 @@
+package factory_method;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
